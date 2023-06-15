481dfa888f0470370507bfb3b5ebbcc6a9c59a1a
Zablon Samba <zablonsamba@gmail.com>
2023-06-15 03:42:14 -0700

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int main()
+/**
+  *main - Starting point
+  *
+  *Description: The puts function will print the string of characters
+  *in the brackets
+  *
+  *Return: Always 0
+  */
+
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n);
+	puts("\"Programming is like building a multilingual puzzle\n");
 
 	return (0);
 }
