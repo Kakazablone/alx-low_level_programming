ac8a8eb5bb527eb5ad1606a0510215c698b7acc4
Zablon Samba <zablonsamba@gmail.com>
2023-06-15 08:52:55 -0700

    Use unistd header to print string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 int main(void)
 {
 
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	write(STDERR_FILENO, str, sizeof(str));
 
