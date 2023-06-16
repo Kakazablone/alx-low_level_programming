3153b26e08239e60993dd5d0fad88508973505f4
Zablon Samba <zablonsamba@gmail.com>
2023-06-16 12:14:57 -0700

    prints all possible combinations of two two-digit numbers.

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,7 +15,7 @@ int main(void)
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j < 100; j++)
+		for (j = i + 1; j < 100; j++)
 		{
 			putchar((i / 10) + '0');
 			putchar((i % 10) + '0');
@@ -23,7 +23,7 @@ int main(void)
 			putchar((j / 10) + '0');
 			putchar((j % 10) + '0');
 
-			if (i < 99 || j < 99)
+			if (i < 98 || j < 99)
 			{
 				putchar (',');
 				putchar (' ');
