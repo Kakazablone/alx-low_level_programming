0704460f7dfeb85f4edc7cbfc9dc6dadf759040c
Zablon Samba <zablonsamba@gmail.com>
2023-06-20 14:39:58 -0700

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+  *jack_bauer - prints every minute of the day of Jack Bauer, starting from
+  *00:00 to 23:59.
+  *
+  *Description: prints every minute of the day of Jack Bauer,
+  *starting from 00:00 to 23:59.
+  *
+  *Return: jack_bauer
+  */
+
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 23; i++)
+	{
+		for (j = i + 1; j <= 59; j++)
+		{
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int a);
 int print_last_digit(int n);
+void jack_bauer(void);
 
 #endif
