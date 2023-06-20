3d952173f1f063f459da20c528f944656ebd128e
Zablon Samba <zablonsamba@gmail.com>
2023-06-20 14:45:17 -0700

    prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,7 +16,7 @@ void jack_bauer(void)
 
 	for (i = 0; i <= 23; i++)
 	{
-		for (j = i + 1; j <= 59; j++)
+		for (j = 0; j <= 59; j++)
 		{
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
