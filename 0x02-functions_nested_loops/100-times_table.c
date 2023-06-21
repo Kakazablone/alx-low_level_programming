f527716b160266649f32586562aaa13c165c90d6
Zablon Samba <zablonsamba@gmail.com>
2023-06-20 18:08:06 -0700

    prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -33,7 +33,7 @@ void print_times_table(int n)
 					_putchar((g / 10) + '0');
 					_putchar((g % 10) + '0');
 				}
-				else if (k >= 100)
+				else if (g >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
