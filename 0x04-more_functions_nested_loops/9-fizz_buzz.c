b59efc3e3ef49293eb7d960883ac7162e6a7e8c7
Zablon Samba <zablonsamba@gmail.com>
2023-06-22 06:36:06 -0700

    multiples of three print Fizz instead of the number and for the multiples of five print Buzz. For numbers which are multiples of both three and five print FizzBuzz.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+  *fizz_b - the test
+  *
+  *@n: checks multiples
+  *
+  *Return: 0
+  */
+
+void fizz_b(int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+
+	}
+}
---
+++ 0x04-more_functions_nested_loops/9-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  *main - checks fizz buzz
+  *
+  *Return: 0
+  */
+
+int main(void)
+{
+	fizz_b(100);
+	return (0);
+}
