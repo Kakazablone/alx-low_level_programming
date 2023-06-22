e63c195b6f127ce85b354a56596e3dab6d72b608
Zablon Samba <zablonsamba@gmail.com>
2023-06-22 08:58:30 -0700

    FB

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -11,7 +11,6 @@ void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
-void fizz_b(int n);
 void print_triangle(int size);
 void print_number(int n);
 
