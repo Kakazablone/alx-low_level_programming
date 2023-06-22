a80f228c8db18b1d6250a70a21214adae6c40a91
Zablon Samba <zablonsamba@gmail.com>
2023-06-22 08:37:58 -0700

    FizzBuzz updated

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,13 +12,9 @@ void fizz_b(int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 		{
 			printf("Fizz ");
 		}
@@ -26,10 +22,15 @@ void fizz_b(int n)
 		{
 			printf("Buzz ");
 		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
 		else
 		{
 			printf("%d ", i);
 		}
 
 	}
+	printf("\n");
 }
