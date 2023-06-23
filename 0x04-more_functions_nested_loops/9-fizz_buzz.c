92a8437ec2e3a65d75c6df0e30b07e70c5ce56d8
Zablon Samba <zablonsamba@gmail.com>
2023-06-22 17:25:24 -0700

    Fizz Buzz always start with more than 1 conditions to be met, compiler reads from the top

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int main(void)
 
 	for (f = 1; f <= 100; f++)
 	{
-		if (f % 3 == 0)
+		if (f % 3 == 0 && f % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (f % 3 == 0)
 		{
 			printf("Fizz");
 		}
 		else if (f % 5 == 0)
 		{
 			printf("Buzz");
-		}
-		else if (f % 3 == 0 && f % 5 == 0)
-		{
-			printf("FizzBuzz");
 		}
 		else
 		{
