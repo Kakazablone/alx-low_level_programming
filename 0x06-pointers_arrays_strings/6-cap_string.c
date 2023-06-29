5daf7b5da3d0aab762ef8e1295adbdc332973e96
Zablon Samba <zablonsamba@gmail.com>
2023-06-29 08:35:11 -0700

    CapString

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,45 +1,35 @@
 #include "main.h"
-
 /**
-  *cap_string - Capitalizes all words in a string
-  *
-  *@str: pointer
-  *
-  *Return: pointer to string
+  *cap_string - function to capitalize
+  *@str: string to be changed
+  *Return: string
   */
-
 char *cap_string(char *str)
 {
-	int indx;
+	int cap = 0;
 
-	indx = 0;
-
-	while (str[indx] != '\0')
+	while (str[cap])
 	{
-		if (str[indx] == 0 && str[indx] >= 97 && str[0] <= 122)
-		{
-			str[indx] = str[indx] - 32;
-		}
-		if (str[indx] == ' '
-				|| str[indx] == '\t'
-				|| str[indx] == '\n'
-				|| str[indx] == ','
-				|| str[indx] == ';'
-				|| str[indx] == '!'
-				|| str[indx] == '"'
-				|| str[indx] == ')'
-				|| str[indx] == '.'
-				|| str[indx] == '?'
-				|| str[indx] == '('
-				|| str[indx] == '{'
-				|| str[indx] == '}')
-		{
-			if (str[indx + 1] >= 97 && str[indx + 1] <= 122)
-			{
-				str[indx + 1] = str[indx + 1] - 32;
-			}
-		}
-		indx++;
+		while (!(str[cap] >= 'a' && str[cap] <= 'z'))
+
+			cap++;
+
+		if (str[cap - 1] == ' ' ||
+		str[cap - 1] == '\t' ||
+		str[cap - 1] == '\n' ||
+		str[cap - 1] == ',' ||
+		str[cap - 1] == ';' ||
+		str[cap - 1] == '.' ||
+		str[cap - 1] == '!' ||
+		str[cap - 1] == '?' ||
+		str[cap - 1] == '"' ||
+		str[cap - 1] == '(' ||
+		str[cap - 1] == ')' ||
+		str[cap - 1] == '{' ||
+		str[cap - 1] == '}' ||
+		cap == 0)
+			str[cap] -= 32;
+		cap++;
 	}
 	return (str);
 }
