2d0218a374ae020b9e347578c3bf8ab8a382d3d1
Zablon Samba <zablonsamba@gmail.com>
2023-07-03 05:16:20 -0700

    prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  *print_chessboard - prints the chessboard
+  *
+  *@a: the 2d array of chars
+  */
+
+void print_chessboard(char (*a)[8])
+{
+	int y, x;
+
+	y = 0;
+	while (y < 8)
+	{
+		x = 0;
+		while (x < 8)
+		{
+			_putchar(a[y][x]);
+			x++;
+		}
+		_putchar('\n');
+		y++;
+	}
+}
