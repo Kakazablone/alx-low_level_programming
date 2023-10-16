7040585a6830f8f9d9aba87896636749ad195d40
Zablon Samba <zablonsamba@gmail.com>
2023-10-16 01:31:53 -0700

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Kakazablone/alx-low_level_programming/tree/master/0x18-dynamic_libraries/makewin.so
+wget -P /tmp https://github.com/Kakazablone/alx-low_level_programming/blob/master/0x18-dynamic_libraries/makewin.so
 export LD_PRELOAD=/tmp/makewin.so
