f8bf819984d8d474c996da3f171b9c13592e9c67
Zablon Samba <zablonsamba@gmail.com>
2023-10-18 01:12:02 -0700

    Make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget https://github.com/Kakazablone/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/$PWD/libtest.so
+wget /tmp https://github.com/Kakazablone/alx-low_level_programming/blob/master/0x18-dynamic_libraries/randlib.so
+export LD_PRELOAD=/tmp/randlib.so
+
