ca78633f370c7028bb5801913a95731d428b67ab
Zablon Samba <zablonsamba@gmail.com>
2023-12-20 22:49:18 +0300

    Documentation

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
 def island_perimeter(grid):
     """
     Takes a grid with an island in it and calculates the perimeter.
